b6aa840536bacd509a4a15f5109d8c75a0a984eb
Uyiosa Idahosa <uyiosa.idahosa@med.uniben.edu>
2024-12-13 16:26:03 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -15,7 +15,7 @@ echo -e "${green}1. PVE开启直通+CPU硬盘温度显示,风扇转速+一键开
 echo -e "${green}2. PVE一键升级PVE，lxc换源，去掉无效订阅${plain}"
 echo -e "${green}3. 开启ssh+BBR+root登录+密码设置(默认密码：dahuilang)${plain}"
 echo -e "${green}4. (centos、ubuntu、debian、alpine)一键开启SSH${plain}"
-echo -e "${green}5. 黑群晖cpu正确识别${plain}"
+echo -e "${green}5. 1黑群晖cpu正确识别${plain}"
 echo -e "${green}6. 黑群晖自动挂载洗白(挂载目录/tmp/boot)${plain}"
 echo -e "${green}7. 一键设置交换虚拟分区${plain}"
 read -p "请输入要执行的操作编号：" operation
